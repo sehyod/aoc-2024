7023b3d90b175d621ece7c08c678f648f5fdc6ea
Mathis Pierron <mathis.pierron@yld.io>
2024-12-01 11:29:22 +0000

    Cleanup day 1 code

--- day1/part1.ts
+++ day1/part1.ts
@@ -1,25 +1,34 @@
 import {BinaryHeap} from "@std/data-structures";
 
-const input = await Deno.readTextFile("./input");
+function part1(input: string) {
+  const left = new BinaryHeap<number>();
+  const right = new BinaryHeap<number>();
 
-const left = new BinaryHeap<number>();
-const right = new BinaryHeap<number>();
+  input.split("\n").forEach((line) => {
+    if (!line) {
+      return;
+    }
+    const regex = /\d+/g;
+    const matches = line.match(regex)!;
+    left.push(-matches[0]);
+    right.push(-matches[1]);
+  });
 
-input.split("\n").forEach((line) => {
-  if (!line) {
-    return;
+  let sum = 0;
+
+  for (const leftLocId of left.drain()) {
+    const rightLocId = right.pop()!;
+    sum += Math.abs(leftLocId - rightLocId);
   }
-  const regex = /\d+/g;
-  const matches = line.match(regex)!;
-  left.push(-matches[0]);
-  right.push(-matches[1]);
-});
 
-let sum = 0;
-
-for (const leftLocId of left.drain()) {
-  const rightLocId = right.pop()!;
-  sum += Math.abs(leftLocId - rightLocId);
+  return sum;
 }
 
-console.log(sum);
+async function main() {
+  const input = await Deno.readTextFile("./input");
+  console.log(part1(input));
+}
+
+if (import.meta.main) {
+  await main();
+}
--- day1/part2.ts
+++ day1/part2.ts
@@ -1,22 +1,28 @@
-const input = await Deno.readTextFile("./input");
+function part2(input: string) {
+  const left: Array<number> = [];
+  const right: Record<number, number> = {};
 
-const left: Array<number> = [];
-const right: Record<number, number> = {};
+  input.split("\n").forEach((line) => {
+    if (!line) {
+      return;
+    }
+    const regex = /\d+/g;
+    const matches = line.match(regex)!;
+    left.push(parseInt(matches[0]));
 
-input.split("\n").forEach((line) => {
-  if (!line) {
-    return;
-  }
-  const regex = /\d+/g;
-  const matches = line.match(regex)!;
-  left.push(parseInt(matches[0]));
+    const rightValue = parseInt(matches[1]);
+    if (!(rightValue in right)) right[rightValue] = 0;
+    right[rightValue] += 1;
+  });
 
-  const rightValue = parseInt(matches[1]);
-  if (!(rightValue in right)) right[rightValue] = 0;
-  right[rightValue] += 1;
-});
+  return left.map((leftValue) =>
+    leftValue in right ? leftValue * right[leftValue] : 0
+  )
+    .reduce((sum, currentValue) => sum + currentValue, 0);
+}
 
-console.log(
-  left.map((leftValue) => leftValue in right ? leftValue * right[leftValue] : 0)
-    .reduce((sum, currentValue) => sum + currentValue, 0),
-);
+async function main() {
+  const input = await Deno.readTextFile("./input");
+  console.log(part2(input));
+}
+if (import.meta.main) await main();
