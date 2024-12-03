53999a268a96bca4bef5c4e5c36b611b28f8bdc2
sehyod <mathis.pierron@laposte.net>
2024-12-03 06:51:13 +0000

    feat: Day 3 part 1

---
+++ day3/day3_test.ts
@@ -0,0 +1,19 @@
+import { expect } from "@std/expect";
+import { describe, it } from "@std/testing/bdd";
+import { part1 } from "./part1.ts";
+import { part2 } from "./part2.ts";
+
+const exampleInput =
+  `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
+
+describe("Day 1", () => {
+  it("Part 1", () => {
+    const res = part1(exampleInput);
+    expect(res).toBe(161);
+  });
+
+  it("Part 2", () => {
+    const res = part2(exampleInput);
+    expect(res).toBe(31);
+  });
+});
