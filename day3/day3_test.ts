1c23606dc646225881121bdb3cd5f980fa2e6f79
sehyod <mathis.pierron@laposte.net>
2024-12-03 07:04:10 +0000

    fix: typo

--- day3/day3_test.ts
+++ day3/day3_test.ts
@@ -3,7 +3,7 @@ import { describe, it } from "@std/testing/bdd";
 import { part1 } from "./part1.ts";
 import { part2 } from "./part2.ts";
 
-describe("Day 1", () => {
+describe("Day 3", () => {
   it("Part 1", () => {
     const exampleInput =
       "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
