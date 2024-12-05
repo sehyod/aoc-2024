27163131c7052aa3d0ce25db0431cdfa85deedb0
sehyod <mathis.pierron@laposte.net>
2024-12-05 08:35:10 +0000

    feat: Day 5 part 1

---
+++ day5/day5_test.ts
@@ -0,0 +1,38 @@
+import { expect } from "@std/expect";
+import { describe, it } from "@std/testing/bdd";
+import { part1 } from "./part1.ts";
+const exampleInput = `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`;
+
+describe("Day 5", () => {
+  it("Part 1", () => {
+    const res = part1(exampleInput);
+    expect(res).toBe(143);
+  });
+  });
---
+++ day5/part1.ts
@@ -0,0 +1,45 @@
+function parseInput(input: string) {
+  const dependencies: Record<number, number[]> = {};
+  const pages: number[][] = [];
+
+  input.split("\n").forEach((line) => {
+    if (line.includes("|")) {
+      const [a, b] = line.split("|").map((n) => parseInt(n));
+      if (!(b in dependencies)) {
+        dependencies[b] = [];
+      }
+      dependencies[b].push(a);
+    } else if (line) {
+      pages.push(line.split(",").map((n) => parseInt(n)));
+    }
+  });
+
+  return { dependencies, pages };
+}
+
+function compareArrays<T>(arr1: T[], arr2: T[]) {
+  return arr1.length === arr2.length &&
+    arr1.every((value, index) => arr2[index] === value);
+}
+
+export function part1(input: string) {
+  const { dependencies, pages } = parseInput(input);
+
+  return pages
+    .filter((line) =>
+      compareArrays(
+        line,
+        line.toSorted((a, b) => (dependencies[b] || []).includes(a) ? -1 : 1),
+      )
+    ).map((line) => line[Math.floor(line.length / 2)])
+    .reduce((a, b) => a + b, 0);
+}
+
+async function main() {
+  const input = await Deno.readTextFile("./input");
+  console.log(part1(input));
+}
+
+if (import.meta.main) {
+  await main();
+}
