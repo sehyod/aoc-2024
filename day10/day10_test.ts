317f20f18cec376604914176aa9af1bae9f8d010
sehyod <mathis.pierron@laposte.net>
2024-12-10 07:15:20 +0000

    feat: Day 10 part 2

--- day10/day10_test.ts
+++ day10/day10_test.ts
@@ -1,6 +1,8 @@
 import { expect } from "@std/expect";
 import { describe, it } from "@std/testing/bdd";
 import { part1 } from "./part1.ts";
+import { part2 } from "./part2.ts";
+
 const exampleInput = `89010123
 78121874
 87430965
@@ -15,4 +17,9 @@ describe("Day 10", () => {
     const res = part1(exampleInput);
     expect(res).toBe(36);
   });
+
+  it("Part 2", () => {
+    const res = part2(exampleInput);
+    expect(res).toBe(81);
   });
+});
