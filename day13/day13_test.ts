29ff3345359003628a4e10c01223aa09ef458508
sehyod <mathis.pierron@laposte.net>
2024-12-13 19:32:22 +0000

    feat: Day 13 part 1

---
+++ day13/day13_test.ts
@@ -0,0 +1,25 @@
+import { expect } from "@std/expect";
+import { describe, it } from "@std/testing/bdd";
+import { part1 } from "./part1.ts";
+const exampleInput = `Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279`;
+
+describe("Day 13", () => {
+  it("Part 1", () => {
+    const res = part1(exampleInput);
+    expect(res).toBe(480);
+  });
+  });
---
+++ day13/part1.ts
@@ -0,0 +1,46 @@
+export function part1(input: string) {
+  const machines: [[number, number], [number, number], [number, number]][] = [];
+  const lines = input
+    .split("\n")
+    .filter(Boolean);
+
+  let i = 0;
+  let machine = [];
+  while (i < lines.length) {
+    const l = lines[i];
+    const [[x], [y]] = l.matchAll(/\d+/g);
+    machine.push([parseInt(x), parseInt(y)]);
+    if (machine.length === 3) {
+      machines.push(
+        machine as [[number, number], [number, number], [number, number]],
+      );
+      machine = [];
+    }
+
+    i += 1;
+  }
+
+  return machines
+    .map(([[xa, ya], [xb, yb], [x, y]]) => {
+      const inverseDet = xa * yb - ya * xb;
+      const a = yb * x - xb * y;
+      const b = xa * y - ya * x;
+
+      if (a % inverseDet == 0 && b % inverseDet == 0) {
+        return [a / inverseDet, b / inverseDet];
+      } else {
+        return null;
+      }
+    })
+    .filter<number[]>((v) => !!v)
+    .reduce((s, [a, b]) => s + 3 * a + b, 0);
+}
+
+async function main() {
+  const input = await Deno.readTextFile("./input");
+  console.log(part1(input));
+}
+
+if (import.meta.main) {
+  await main();
+}
