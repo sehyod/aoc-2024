36ff02a1fd686b554485b886aa5ecc5c85e34a86
sehyod <mathis.pierron@laposte.net>
2024-12-15 07:13:08 +0000

    feat: Day 15 part 1

---
+++ day15/day15_test.ts
@@ -0,0 +1,47 @@
+import { expect } from "@std/expect";
+import { describe, it } from "@std/testing/bdd";
+import { part1 } from "./part1.ts";
+
+const smallExampleInput = `########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<`;
+
+const largeExampleInput = `##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^`;
+
+describe("Day 15", () => {
+  it("Part 1 - Small example", () => {
+    const res = part1(smallExampleInput);
+    expect(res).toBe(2028);
+  });
+  it("Part 1 - Large example", () => {
+    const res = part1(largeExampleInput);
+    expect(res).toBe(10092);
+  });
+});
---
+++ day15/part1.ts
@@ -0,0 +1,63 @@
+const DIR: Record<string, [number, number]> = {
+  ">": [0, 1],
+  "<": [0, -1],
+  "^": [-1, 0],
+  "v": [1, 0],
+};
+
+export function part1(input: string) {
+  const grid: string[][] = [];
+
+  let pos: [number, number] | null = null;
+
+  let [map, instr] = input.split("\n\n");
+  instr = instr.replaceAll("\n", "");
+  map.split("\n").forEach((line, i) => {
+    const row: string[] = [];
+    [...line].forEach((c, j) => {
+      if (c === "@") {
+        pos = [i, 2 * j];
+        row.push("..");
+      } else if (c === "O") {
+        row.push("[]");
+      } else {
+        row.push(c);
+      }
+    });
+    grid.push(row);
+  });
+
+  [...instr].forEach((c) => {
+    const [dx, dy] = DIR[c];
+    const [x, y] = pos!;
+    let i = 1;
+    while (!"#.".includes(grid[x + i * dx][y + i * dy])) {
+      i++;
+    }
+    if (grid[x + i * dx][y + i * dy] === "#") {
+      return;
+    }
+    if (i > 1) {
+      grid[x + dx][y + dy] = ".";
+      grid[x + i * dx][y + i * dy] = "O";
+    }
+    pos = [x + dx, y + dy];
+  });
+
+  return grid
+    .map((r, i) =>
+      r
+        .map((c, j) => c === "O" ? 100 * i + j : 0)
+        .reduce((a, b) => a + b, 0)
+    )
+    .reduce((a, b) => a + b, 0);
+}
+
+async function main() {
+  const input = await Deno.readTextFile("./input");
+  console.log(part1(input));
+}
+
+if (import.meta.main) {
+  await main();
+}
