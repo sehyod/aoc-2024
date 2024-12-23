639ea875a8cf68e362d07aaadb52a0e6d9001973
Mathis Pierron <mathis.pierron@yld.io>
2024-12-23 15:51:29 +0000

    feat: Day 18 part 2

--- day18/day18_test.ts
+++ day18/day18_test.ts
@@ -1,6 +1,7 @@
 import { expect } from "@std/expect";
 import { describe, it } from "@std/testing/bdd";
 import { part1 } from "./part1.ts";
+import { part2 } from "./part2.ts";
 
 const exampleInput = `5,4
 4,2
@@ -33,5 +34,9 @@ describe("Day 18", () => {
     const res = part1(exampleInput, 7, 12);
     expect(res).toBe(22);
   });
+
+  it("Part 2", () => {
+    const res = part2(exampleInput, 7, 12);
+    expect(res).toBe("6,1");
   });
 });
